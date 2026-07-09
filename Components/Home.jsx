540e9cd3d925d4b59be200caf3a4233abefbf464
Abdul Rashid Chhimpa <110084078+Abdul-Rashid-Chhimpa@users.noreply.github.com>
2026-07-10 01:27:43 +0530

    Update Home.jsx

--- Components/Home.jsx
+++ Components/Home.jsx
@@ -4,11 +4,11 @@ import RandomImg from './RandomImg';
 
 const Home = () => {
     const images = [
-  "/images/img1.png",
-  "/images/img2.png",
-  "/images/img3.png",
-  "/images/img4.png",
-  "/images/img5.png",
+  "01.jpeg",
+  "02.jpeg",
+  "03.jpg",
+  "04.jpg",
+  "05.jpg",
 ];
     return (
         <>
