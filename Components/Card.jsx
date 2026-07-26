d536d4346f420ae7134180083380104111b8bd6d
Abdul Rashid Chhimpa <110084078+Abdul-Rashid-Chhimpa@users.noreply.github.com>
2026-07-26 00:24:46 -0700

    Update Card.jsx

--- Components/Card.jsx
+++ Components/Card.jsx
@@ -14,6 +14,7 @@ import {
   Settings,
   Package,
 } from "lucide-react";
+// import { CartContext } from "../../context/CartContext";
 import { CartContext } from "../../context/CartContext";
 
 // ======================================================
