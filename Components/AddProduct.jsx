b13f99e1dd47bcd04858845719422829e318184c
Abdul Rashid Chhimpa <110084078+Abdul-Rashid-Chhimpa@users.noreply.github.com>
2026-08-16 07:11:46 -0700

    Update AddProduct.jsx

--- Components/AddProduct.jsx
+++ Components/AddProduct.jsx
@@ -182,7 +182,7 @@ const AddProduct = () => {
     } catch (err) {
       console.log(err);
       alert(err.response?.data?.message || "Product Add Failed");
-    } font-medium {
+    } {
       setLoading(false);
     }
   };
