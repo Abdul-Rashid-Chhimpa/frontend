1521e06ffaf4d7ddaaec2d551ad6d8ba93a3137b
Abdul Rashid Chhimpa <110084078+Abdul-Rashid-Chhimpa@users.noreply.github.com>
2026-09-12 22:53:43 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -76,6 +76,7 @@ function App() {
           />
 
           {/* ================= ADMIN-ONLY ROUTES ================= */}
+          
           <Route
             path="/add-product"
             element={
